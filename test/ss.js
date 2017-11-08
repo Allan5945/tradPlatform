84443bcf3f4f081365cd8279441e427d619371da
allan5945 <allan5945@163.com>
2017-11-08 10:07:23 +0800

    ---...

---
+++ test/ss.js
@@ -0,0 +1,152 @@
+[
+ {
+    "name": "上海 Top10",
+    "type": "lines",
+    "coordinateSystem": "bmap",
+    "zlevel": 2,
+    "effect": {
+        "show": true,
+        "period": 6,
+        "trailLength": 0,
+        "symbol": "path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z",
+        "symbolSize": 15
+    },
+    "lineStyle": {"normal": {"color": "#ffa022", "width": 1, "opacity": 0.4, "curveness": 0.2}},
+    "data": [{
+        "fromName": "上海",
+        "toName": "包头",
+        "coords": [[121.4648, 31.2891], [110.3467, 41.4899]]
+    }, {"fromName": "上海", "toName": "昆明", "coords": [[121.4648, 31.2891], [102.9199, 25.4663]]}, {
+        "fromName": "上海",
+        "toName": "广州",
+        "coords": [[121.4648, 31.2891], [113.5107, 23.2196]]
+    }, {"fromName": "上海", "toName": "郑州", "coords": [[121.4648, 31.2891], [113.4668, 34.6234]]}, {
+        "fromName": "上海",
+        "toName": "长春",
+        "coords": [[121.4648, 31.2891], [125.8154, 44.2584]]
+    }, {"fromName": "上海", "toName": "重庆", "coords": [[121.4648, 31.2891], [107.7539, 30.1904]]}, {
+        "fromName": "上海",
+        "toName": "长沙",
+        "coords": [[121.4648, 31.2891], [113.0823, 28.2568]]
+    }, {"fromName": "上海", "toName": "北京", "coords": [[121.4648, 31.2891], [116.4551, 40.2539]]}, {
+        "fromName": "上海",
+        "toName": "丹东",
+        "coords": [[121.4648, 31.2891], [124.541, 40.4242]]
+    }, {"fromName": "上海", "toName": "大连", "coords": [[121.4648, 31.2891], [122.2229, 39.4409]]}]
+}, {
+    "name": "上海 Top10",
+    "type": "effectScatter",
+    "coordinateSystem": "bmap",
+    "zlevel": 2,
+    "rippleEffect": {"brushType": "stroke"},
+    "label": {"normal": {"show": true, "position": "right", "formatter": "{b}"}},
+    "showEffectOn": "render",
+    "itemStyle": {"normal": {"color": "#ffa022"}},
+    "data": [{"name": "包头", "value": [110.3467, 41.4899, 95]}, {
+        "name": "昆明",
+        "value": [102.9199, 25.4663, 90]
+    }, {"name": "广州", "value": [113.5107, 23.2196, 80]}, {
+        "name": "郑州",
+        "value": [113.4668, 34.6234, 70]
+    }, {"name": "长春", "value": [125.8154, 44.2584, 60]}, {
+        "name": "重庆",
+        "value": [107.7539, 30.1904, 50]
+    }, {"name": "长沙", "value": [113.0823, 28.2568, 40]}, {
+        "name": "北京",
+        "value": [116.4551, 40.2539, 30]
+    }, {"name": "丹东", "value": [124.541, 40.4242, 20]}, {"name": "大连", "value": [122.2229, 39.4409, 10]}]
+}, {
+    "name": "广州 Top10",
+    "type": "effectScatter",
+    "coordinateSystem": "bmap",
+    "zlevel": 2,
+    "rippleEffect": {"brushType": "stroke"},
+    "label": {"normal": {"show": true, "position": "right", "formatter": "{b}"}},
+    "showEffectOn": "render",
+    "itemStyle": {"normal": {"color": "#46bee9"}},
+    "data": [{"name": "广州", "value": [113.5107, 23.2196, 100]}]
+}, {
+    "name": "广州 Top10",
+    "type": "lines",
+    "coordinateSystem": "bmap",
+    "zlevel": 1,
+    "effect": {"show": true, "period": 6, "trailLength": 0.7, "color": "#fff", "symbolSize": 3},
+    "lineStyle": {"normal": {"color": "#46bee9", "width": 0, "curveness": 0.2}},
+    "data": [{
+        "fromName": "广州",
+        "toName": "福州",
+        "coords": [[113.5107, 23.2196], [119.4543, 25.9222]]
+    }, {"fromName": "广州", "toName": "太原", "coords": [[113.5107, 23.2196], [112.3352, 37.9413]]}, {
+        "fromName": "广州",
+        "toName": "长春",
+        "coords": [[113.5107, 23.2196], [125.8154, 44.2584]]
+    }, {"fromName": "广州", "toName": "重庆", "coords": [[113.5107, 23.2196], [107.7539, 30.1904]]}, {
+        "fromName": "广州",
+        "toName": "西安",
+        "coords": [[113.5107, 23.2196], [109.1162, 34.2004]]
+    }, {"fromName": "广州", "toName": "成都", "coords": [[113.5107, 23.2196], [103.9526, 30.7617]]}, {
+        "fromName": "广州",
+        "toName": "常州",
+        "coords": [[113.5107, 23.2196], [119.4543, 31.5582]]
+    }, {"fromName": "广州", "toName": "北京", "coords": [[113.5107, 23.2196], [116.4551, 40.2539]]}, {
+        "fromName": "广州",
+        "toName": "北海",
+        "coords": [[113.5107, 23.2196], [109.314, 21.6211]]
+    }, {"fromName": "广州", "toName": "海口", "coords": [[113.5107, 23.2196], [110.3893, 19.8516]]}]
+}, {
+    "name": "广州 Top10",
+    "type": "lines",
+    "coordinateSystem": "bmap",
+    "zlevel": 2,
+    "effect": {
+        "show": true,
+        "period": 6,
+        "trailLength": 0,
+        "symbol": "path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z",
+        "symbolSize": 15
+    },
+    "lineStyle": {"normal": {"color": "#46bee9", "width": 1, "opacity": 0.4, "curveness": 0.2}},
+    "data": [{
+        "fromName": "广州",
+        "toName": "福州",
+        "coords": [[113.5107, 23.2196], [119.4543, 25.9222]]
+    }, {"fromName": "广州", "toName": "太原", "coords": [[113.5107, 23.2196], [112.3352, 37.9413]]}, {
+        "fromName": "广州",
+        "toName": "长春",
+        "coords": [[113.5107, 23.2196], [125.8154, 44.2584]]
+    }, {"fromName": "广州", "toName": "重庆", "coords": [[113.5107, 23.2196], [107.7539, 30.1904]]}, {
+        "fromName": "广州",
+        "toName": "西安",
+        "coords": [[113.5107, 23.2196], [109.1162, 34.2004]]
+    }, {"fromName": "广州", "toName": "成都", "coords": [[113.5107, 23.2196], [103.9526, 30.7617]]}, {
+        "fromName": "广州",
+        "toName": "常州",
+        "coords": [[113.5107, 23.2196], [119.4543, 31.5582]]
+    }, {"fromName": "广州", "toName": "北京", "coords": [[113.5107, 23.2196], [116.4551, 40.2539]]}, {
+        "fromName": "广州",
+        "toName": "北海",
+        "coords": [[113.5107, 23.2196], [109.314, 21.6211]]
+    }, {"fromName": "广州", "toName": "海口", "coords": [[113.5107, 23.2196], [110.3893, 19.8516]]}]
+}, {
+    "name": "广州 Top10",
+    "type": "effectScatter",
+    "coordinateSystem": "bmap",
+    "zlevel": 2,
+    "rippleEffect": {"brushType": "stroke"},
+    "label": {"normal": {"show": true, "position": "right", "formatter": "{b}"}},
+    "showEffectOn": "render",
+    "itemStyle": {"normal": {"color": "#46bee9"}},
+    "data": [{"name": "福州", "value": [119.4543, 25.9222, 95]}, {
+        "name": "太原",
+        "value": [112.3352, 37.9413, 90]
+    }, {"name": "长春", "value": [125.8154, 44.2584, 80]}, {
+        "name": "重庆",
+        "value": [107.7539, 30.1904, 70]
+    }, {"name": "西安", "value": [109.1162, 34.2004, 60]}, {
+        "name": "成都",
+        "value": [103.9526, 30.7617, 50]
+    }, {"name": "常州", "value": [119.4543, 31.5582, 40]}, {
+        "name": "北京",
+        "value": [116.4551, 40.2539, 30]
+    }, {"name": "北海", "value": [109.314, 21.6211, 20]}, {"name": "海口", "value": [110.3893, 19.8516, 10]}]
+}]
