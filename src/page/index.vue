3b8a4b0f84553e41526d0a3f2bcfc171fc04518c
allan5945 <allan5945@163.com>
2017-12-07 20:52:41 +0800

    修改配置文件

--- src/page/index.vue
+++ src/page/index.vue
@@ -17,9 +17,9 @@
          <infPanel></infPanel>
         <paySuccess @cancel="payDialog = false" v-show="payDialog"></paySuccess>
         <airlineDetailPayAfter v-show="detailShow2" @transShow='transShow2'  @closeThis="closeThis"></airlineDetailPayAfter>
-        <myIndex></myIndex>
+        <!--<myIndex></myIndex>-->
         <routeNetwork></routeNetwork>
-         <myIndexFzz></myIndexFzz>
+         <!--<myIndexFzz></myIndexFzz>-->
     </div>
 </template>
 
@@ -275,10 +275,4 @@
 </script>
 
 <style lang="scss">
-    .dialog-enter-active, .dialog-leave-active {
-     transition: opacity .3s;
-}
-.dialog-enter, .dialog-leave-to{
-    opacity: 0;
-}
 </style>
