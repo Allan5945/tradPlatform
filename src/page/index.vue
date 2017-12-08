b04ed360540d94e579005bdb92c6fe89b568f6b4
zhangshuai <zhangshuai@taimei-air.com>
2017-12-08 18:04:31 +0800

    merge

--- src/page/index.vue
+++ src/page/index.vue
@@ -17,7 +17,7 @@
          <infPanel></infPanel>
         <paySuccess @cancel="payDialog = false" v-show="payDialog"></paySuccess>
         <airlineDetailPayAfter v-show="detailShow2" @transShow='transShow2'  @closeThis="closeThis"></airlineDetailPayAfter>
-       <myIndex></myIndex>
+       <!-- <myIndex></myIndex> -->
         <routeNetwork></routeNetwork>
          <!--<myIndexFzz></myIndexFzz>-->
     </div>
